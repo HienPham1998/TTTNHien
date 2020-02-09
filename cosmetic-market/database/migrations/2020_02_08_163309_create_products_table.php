45cb8fb79f151eae43ae9dd098c9e4d3d04fe550
HienPham1998 <54305818+HienPham1998@users.noreply.github.com>
2020-02-09 12:01:03 +0700

    first commit

---
+++ cosmetic-market/database/migrations/2020_02_08_163309_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('category_id');
+            $table->integer('provider_id');
+            $table->double('sale');
+            $table->double('price');
+            $table->string('quantity');
+            $table->string("image");
+            $table->string('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
