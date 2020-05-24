304e0504c91960e13a14c2c4c5fb45366ee81900
iarim <anhdung9700@gmail.com>
2020-05-25 00:03:40 +0700

    update2

--- cosmetic-market/app/Saler.php
+++ cosmetic-market/app/Saler.php
@@ -8,6 +8,6 @@ class Saler extends Model
 {
     public function product()
     {
-        return $this->belongsToMany('App\Product');
+        return $this->belongsToMany('App\Product','product_saler', 'product_id', 'saler_id')->withPivot('discount', 'unit_price', 'quantity', 'description', 'image', 'ingredient', 'manufacturing_date', 'expiry_date')->withTimestamps();;
     }
 }
