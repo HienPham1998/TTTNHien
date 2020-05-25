b7784a91cef272411bd7157d37baffe1fbabe53b
HienPham1998 <hienktpm1@gmail.com>
2020-05-25 22:09:22 +0700

    update

--- cosmetic-market/app/Category.php
+++ cosmetic-market/app/Category.php
@@ -8,6 +8,6 @@ class Category extends Model
 {
     //
     public function category(){
-        return $this->hasMany('App\Category');
+        return $this->belongsTo('App\CategoryType');
     }
 }
--- cosmetic-market/app/CategoryType.php
+++ cosmetic-market/app/CategoryType.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class CategoryType extends Model
 {
     //
-     public function categoryType(){
-        return $this->hasMany('App\CategoryType');
+     public function subcategory(){
+        return $this->hasMany('App\Category');
     }
 }
